3d65a36a7b385fd20cc0b5711c259fe693286852
Enrique Hernandez <enrique_inf@hotmail.com>
2021-10-08 22:02:09 -0500

    primer commit

---
+++ Enitdades/Escuela.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreEscuela.Enitdades
+{
+    public class Escuela
+    {
+        string nombre;
+        public string Nombre
+        {
+            get { return "Copia:" + nombre; }
+            set { nombre = value.ToUpper(); }
+        }
+
+        public int añoDeCreacion { get; set; }
+
+        public string Pais { get; set; }
+
+        public string Ciudad { get; set; }
+
+        public TiposEscuela TipoEscuela { get; set; }
+
+        //constructor de la clase o metodo
+        public Escuela(string nombre, int año)
+        {
+            this.nombre = nombre;
+            añoDeCreacion = año;
+        }
+
+        //En C# todo es un objeto
+        public override string ToString()
+        {
+            return $"Nombre: {Nombre}, Tipo: {TipoEscuela} \n Pais: {Pais}, Ciudad: {Ciudad}";
+        }
+
+        //public Escuela(string nombre, int año) => (Nombre, añoDeCreacion) = (nombre, año);
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using CoreEscuela.Enitdades;
+
+namespace Etapa1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var escuela = new Escuela("Platzi",2012);
+            escuela.Pais = "Mexico";
+            escuela.Ciudad = "Naucalpan";
+            escuela.TipoEscuela = TiposEscuela.Primaria;
+            Console.WriteLine(escuela);
+            Console.ReadLine();
+        }
+    }
+}
